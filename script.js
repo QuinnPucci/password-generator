a14cf0f651609ecde107fccf04a9d95d13771cf7
QuinnPooch <pucciquinn@gmail.com>
2021-11-05 11:12:01 -0600

    added JS, not working

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-// Assignment code here
+var form = document.getElementById("form")
+var rangeEl = document.getElementById("characterRange")
+var upperEl = document.getElementById("upper")
+var numberEl = document.getElementById("number")
+var symbolEl = document.getElementById("symbol")
+
+var LOWERCASE_CHAR_CODE = arrayFromLowToHigh(97,122)
+var UPPERCASE_CHAR_CODE = arrayFromLowToHigh(65,90)
+var NUMBER_CHAR_CODE = arrayFromLowToHigh(48,57)
+var SYMBOL_CHAR_CODE = arrayFromLowToHigh(33,47)
+var passwordDisplay = document.getElementById("password")
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+form.addEventListener("submit", e=> {
+  e.preventDefault
+  var range = rangeEl.value
+  var upper = upperEl.checked
+  var number = numberEl.checked
+  var symbol = symbolEl.checked
+  generatePassword()
+  var password = generatePassword(range, upper, symbol, number);
+  passwordDisplay.innerText = password
+})
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword(range, upper, symbol, number) {
+ let charCodes = LOWERCASE_CHAR_CODE 
+ if (upper) charCodes = charCodes.concat(UPPERCASE_CHAR_CODE)
+ if (number) charCodes = charCodes.concat(NUMBER_CHAR_CODE)
+ if (symbol) charCodes = charCodes.concat(SYMBOL_CHAR_CODE)
 
-  passwordText.value = password;
-
+ var passwordCharacters = []
+ for  (let i=0; i < rangeEl; i++) {
+   var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+ passwordCharacters.push(String.fromCharCode(characterCode)
+ }
+ return passwordCharacters.join("")
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function arrayFromLowToHigh(low, high) {
+  var array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i) 
+  }
+  return array
+}
+
